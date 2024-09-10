8ce9ffb261a99d8700d7156974f5edd1a942152d
Aimee Pederson <aimee.pederson@gmail.com>
2024-09-10 17:00:10 -0500

    begin data and dataTypes

---
+++ src/data/data.tsx
@@ -0,0 +1,12 @@
+import {
+    HomepageMeta,
+  } from './dataDef';
+  
+/**
+ * Page meta data
+ */
+export const homePageMeta: HomepageMeta = {
+  title: "Aimee Pederson",
+  description:
+    "Aimee Pederson | Frontend Developer, Conversion Rate Optimization Expert, QA Engineer",
+};
---
+++ src/data/dataDef.ts
@@ -0,0 +1,13 @@
+export interface HomepageMeta {
+  title: string;
+  description: string;
+  ogImageUrl?: string;
+  twitterCardType?: "summary" | "summary_large";
+  twitterTitle?: string;
+  twitterSite?: string;
+  twitterCreator?: string;
+  twitterDomain?: string;
+  twitterUrl?: string;
+  twitterDescription?: string;
+  twitterImageUrl?: string;
+}
